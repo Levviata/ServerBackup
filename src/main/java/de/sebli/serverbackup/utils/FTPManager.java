fb9cc8e16fb6d74d5ed8cea47894a982dd18adfe
Levviata (Lev Gomez) <aaronober11@gmail.com>
2025-01-07 12:22:08 +0100

    Straightforward logging
    Use info(string) instead of log(level.info, string) because its more straightforward.

--- src/main/java/de/sebli/serverbackup/utils/FTPManager.java
+++ src/main/java/de/sebli/serverbackup/utils/FTPManager.java
@@ -69,8 +69,8 @@ public class FTPManager {
                 boolean done = ftpClient.storeFile(file.getName(), inputStream);
 
                 // DEBUG
-                Bukkit.getLogger().log(Level.INFO, "(BETA) FTP-DEBUG INFO: " + ftpClient.getReplyString());
-                Bukkit.getLogger().log(Level.INFO,
+                Bukkit.getLogger().info("(BETA) FTP-DEBUG INFO: " + ftpClient.getReplyString());
+                Bukkit.getLogger().info(
                         "Use this info for reporting ftp related bugs. Ignore it if everything is fine.");
 
                 inputStream.close();
