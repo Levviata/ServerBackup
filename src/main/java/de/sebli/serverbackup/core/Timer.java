7fbbf233df5157debb31ca7a8dec8892230f18d3
Levviata <68709348+Levviata@users.noreply.github.com>
2025-01-15 20:43:57 +0100

    Lego deconstruct Timer.java (brain method)
    - Add properties private, static, and final to lists world, days, and times for clarity
    - Add properties private to variables cal and checkMinute for clarity too (maybe not that necessary but wtver)
    
    - Deconstruct/extract brain method (run() method) into 8 methods:
    
    * handleBackupOnScheduledDay()
    * cleanTimeString()
    * createBackupsForWorlds()
    * handleOldBackupDeletion()
    * deleteOldBackups()
    * logBackupDeletionStart()
    * logBackupDeletionEnd()
    * handleBackupLimiter()
    
    - Build strings before passing them to the logger if they are formatted (remove string concoction for String.format)
    
    - Improve logging (consistency and clarity) here and there
    
    - Minimalized logic in getDayName() (rewrite the thing so that its smaller but same purpose)

--- src/main/java/de/sebli/serverbackup/core/Timer.java
+++ src/main/java/de/sebli/serverbackup/core/Timer.java
@@ -10,16 +10,16 @@ import java.util.*;
 import java.util.logging.Level;
 
 import static de.sebli.serverbackup.utils.FileUtil.tryDeleteFile;
+import static de.sebli.serverbackup.utils.TaskUtils.getTasks;
 
 public class Timer implements Runnable {
 
-    List<String> worlds = ServerBackupPlugin.getPluginInstance().getConfig().getStringList("BackupWorlds");
-    List<String> days = ServerBackupPlugin.getPluginInstance().getConfig().getStringList("BackupTimer.Days");
-    List<String> times = ServerBackupPlugin.getPluginInstance().getConfig().getStringList("BackupTimer.Times");
+    private static final List<String> worlds = ServerBackupPlugin.getPluginInstance().getConfig().getStringList("BackupWorlds");
+    private static final List<String> days = ServerBackupPlugin.getPluginInstance().getConfig().getStringList("BackupTimer.Days");
+    private static final List<String> times = ServerBackupPlugin.getPluginInstance().getConfig().getStringList("BackupTimer.Times");
 
-    Calendar cal = Calendar.getInstance();
-
-    short checkMinute = 0;
+    private Calendar cal = Calendar.getInstance();
+    private short checkMinute = 0;
 
     @Override
     public void run() {
@@ -30,164 +30,179 @@ public class Timer implements Runnable {
 
             if (checkMinute != timeCode) {
                 checkMinute = timeCode;
-
-                boolean isBackupDay = days.stream().filter(d -> d.equalsIgnoreCase(getDayName(cal.get(Calendar.DAY_OF_WEEK))))
-                        .findFirst().isPresent();
-
-                if (isBackupDay) {
-                    for (String time : times) {
-                        try {
-                            String[] timeStr = time.split("-");
-
-                            if (timeStr[0].startsWith("0")) {
-                                timeStr[0] = timeStr[0].substring(1);
-                            }
-
-                            if (timeStr[1].startsWith("0")) {
-                                timeStr[1] = timeStr[1].substring(1);
-                            }
-
-                            byte hour = Byte.valueOf(timeStr[0]);
-                            byte minute = Byte.valueOf(timeStr[1]);
-
-                            if ((byte) cal.get(Calendar.HOUR_OF_DAY) == hour && (byte) cal.get(Calendar.MINUTE) == minute) {
-                                for (String world : worlds) {
-                                    Backup backup = new Backup(world, Bukkit.getConsoleSender(),
-                                            !ServerBackupPlugin.getPluginInstance().getConfig().getBoolean("DynamicBackup"));
-                                    backup.create();
-                                }
-                            }
-                        } catch (Exception e) {
-                            ServerBackupPlugin.getPluginInstance().getLogger().log(Level.WARNING,
-                                    "ServerBackup: Automatic Backup failed. Please check that you set the BackupTimer correctly.");
-                        }
-                    }
-                }
+                handleBackupOnScheduledDay();
             }
         }
 
         if (ServerBackupPlugin.getPluginInstance().getConfig().getInt("BackupLimiter") <= 0) {
-            if (cal.get(Calendar.HOUR_OF_DAY) == 0 && cal.get(Calendar.MINUTE) == 0) {
-                if (ServerBackupPlugin.getPluginInstance().getConfig().getInt("DeleteOldBackups") <= 0)
-                    return;
+            handleOldBackupDeletion();
+        } else {
+            handleBackupLimiter();
+        }
 
-                File[] backups = new File(Configuration.backupDestination + "").listFiles();
+        if (OperationHandler.getShutdownProgress() && getTasks().isEmpty()) {
+            ServerBackupPlugin.getPluginInstance().getLogger().info("All tasks finished. Shutting down server...");
+            Bukkit.shutdown();
+        }
+    }
 
-                if (backups.length == 0)
-                    return;
+    private void handleBackupOnScheduledDay() {
+        boolean isBackupDay = days.stream()
+                .anyMatch(d -> d.equalsIgnoreCase(getDayName(cal.get(Calendar.DAY_OF_WEEK))));
 
-                Arrays.sort(backups, Collections.reverseOrder());
+        if (isBackupDay) {
+            for (String time : times) {
+                try {
+                    String[] timeStr = time.split("-");
+                    cleanTimeString(timeStr);
 
-                LocalDate date = LocalDate.now()
-                        .minusDays(ServerBackupPlugin.getPluginInstance().getConfig().getInt("DeleteOldBackups"));
+                    byte hour = Byte.parseByte(timeStr[0]);
+                    byte minute = Byte.parseByte(timeStr[1]);
 
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "ServerBackup | Backup deletion started...");
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
+                    if ((byte) cal.get(Calendar.HOUR_OF_DAY) == hour && (byte) cal.get(Calendar.MINUTE) == minute) {
+                        createBackupsForWorlds();
+                    }
+                } catch (Exception e) {
+                    ServerBackupPlugin.getPluginInstance().getLogger().log(Level.WARNING,
+                            "Automatic Backup failed. Please check that you set the BackupTimer correctly.");
+                }
+            }
+        }
+    }
 
-                long time = System.currentTimeMillis();
+    private void cleanTimeString(String[] timeStr) {
+        if (timeStr[0].startsWith("0")) {
+            timeStr[0] = timeStr[0].substring(1);
+        }
 
-                List<String> backupNames = new ArrayList<>();
+        if (timeStr[1].startsWith("0")) {
+            timeStr[1] = timeStr[1].substring(1);
+        }
+    }
 
-                for (int i = 0; i < backups.length; i++) {
-                    try {
-                        String[] backupDateStr = backups[i].getName().split("-");
-                        LocalDate backupDate = LocalDate.parse(
-                                backupDateStr[1] + "-" + backupDateStr[2] + "-" + backupDateStr[3].split("~")[0]);
-                        String backupName = backupDateStr[6];
+    private void createBackupsForWorlds() {
+        for (String world : worlds) {
+            Backup backup = new Backup(world, Bukkit.getConsoleSender(),
+                    !ServerBackupPlugin.getPluginInstance().getConfig().getBoolean("DynamicBackup"));
+            backup.create();
+        }
+    }
 
-                        if (ServerBackupPlugin.getPluginInstance().getConfig().getBoolean("KeepUniqueBackups")) {
-                            if (!backupNames.contains(backupName)) {
-                                backupNames.add(backupName);
-                                continue;
-                            }
-                        }
+    private void handleOldBackupDeletion() {
+        if (cal.get(Calendar.HOUR_OF_DAY) == 0 && cal.get(Calendar.MINUTE) == 0) {
+            if (ServerBackupPlugin.getPluginInstance().getConfig().getInt("DeleteOldBackups") <= 0) {
+                return;
+            }
 
-                        if (backupDate.isBefore(date.plusDays(1))) {
-                            if (backups[i].exists()) {
-                                backups[i].delete();
+            deleteOldBackups();
+        }
+    }
 
-                                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO,
-                                        "Backup [" + backups[i].getName() + "] removed.");
-                            } else {
-                                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.WARNING,
-                                        "No Backup named '" + backups[i].getName() + "' found.");
-                            }
-                        }
-                    } catch (Exception e) {
+    private void deleteOldBackups() {
+        File[] backups = new File(Configuration.backupDestination).listFiles();
+
+        if (backups.length == 0) return;
+
+        Arrays.sort(backups, Collections.reverseOrder());
+
+        LocalDate date = LocalDate.now()
+                .minusDays(ServerBackupPlugin.getPluginInstance().getConfig().getInt("DeleteOldBackups"));
+
+        logBackupDeletionStart();
+
+        long time = System.currentTimeMillis();
+        List<String> backupNames = new ArrayList<>();
+
+        for (File backup : backups) {
+            if (!backup.isFile()) {
+                continue; // Skip directories
+            }
+
+            try {
+                String[] backupDateStr = backup.getName().split("-");
+                LocalDate backupDate = LocalDate.parse(backupDateStr[1] + "-" + backupDateStr[2] + "-" + backupDateStr[3].split("~")[0]);
+                String backupName = backupDateStr[6];
+
+                if (ServerBackupPlugin.getPluginInstance().getConfig().getBoolean("KeepUniqueBackups") &&
+                        !backupNames.contains(backupName)) {
+
+                        backupNames.add(backupName);
+                        continue;
+                    }
+
+
+                if (backupDate.isBefore(date.plusDays(1))) {
+                    if (backup.exists()) {
+                        tryDeleteFile(backup);
+
+                        String formattedMessage = String.format("Backup [ [%s] ] removed.", backup.getName());
+                        ServerBackupPlugin.getPluginInstance().getLogger().info(formattedMessage);
+                    } else {
+                        String formattedMessage = String.format("No Backup named [ [%s] ] found.", backup.getName());
+                        ServerBackupPlugin.getPluginInstance().getLogger().warning(formattedMessage);
                     }
                 }
-
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "ServerBackup | Backup deletion finished. ["
-                        + Long.valueOf(System.currentTimeMillis() - time) + "ms]");
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
-            }
-        } else {
-            File[] backups = new File(Configuration.backupDestination + "").listFiles();
-            Arrays.sort(backups);
-
-            int dobc = ServerBackupPlugin.getPluginInstance().getConfig().getInt("BackupLimiter");
-            int c = 0;
-
-            while (backups.length > dobc) {
-                if (backups[c].exists()) {
-                    tryDeleteFile(backups[c]);
-
-                    ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO,
-                            "Backup [" + backups[c].getName() + "] removed.");
-                } else {
-                    ServerBackupPlugin.getPluginInstance().getLogger().log(Level.WARNING,
-                            "No Backup named '" + backups[c].getName() + "' found.");
-                }
-
-                c++;
-                dobc++;
+            } catch (Exception e) {
+                String formattedMessage = String.format("Failed to process backup file: [%s]. Reason: %s", backup.getName(), e.getMessage());
+                ServerBackupPlugin.getPluginInstance().getLogger().warning(formattedMessage);
             }
         }
 
-        if (OperationHandler.getShutdownProgress()) {
-            if (OperationHandler.getTasks().size() == 0) {
-                ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO,
-                        "Backup tasks finished. Shutting down server...");
+        logBackupDeletionEnd(time);
+    }
 
-                Bukkit.shutdown();
+    private void logBackupDeletionStart() {
+        // Get the current time in a readable format
+        String startTime = java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss").format(java.time.LocalDateTime.now());
+
+        // Format the message with the start time
+        String formattedMessage = String.format("ServerBackup | Backup deletion started... [ started at [%s] ]", startTime);
+
+        // Log the message
+        ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
+        ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, formattedMessage);
+        ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
+    }
+
+    private void logBackupDeletionEnd(long time) {
+        String formattedMessage = String.format("ServerBackup | Backup deletion finished. [ finished in [%s]ms ]", (System.currentTimeMillis() - time));
+
+        ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
+        ServerBackupPlugin.getPluginInstance().getLogger().info(formattedMessage);
+        ServerBackupPlugin.getPluginInstance().getLogger().log(Level.INFO, "");
+    }
+
+    private void handleBackupLimiter() {
+        File[] backups = new File(Configuration.backupDestination).listFiles();
+        Arrays.sort(Objects.requireNonNull(backups));
+
+        int dobc = ServerBackupPlugin.getPluginInstance().getConfig().getInt("BackupLimiter");
+        int c = 0;
+
+        while (backups.length > dobc) {
+            if (backups[c].exists()) {
+                tryDeleteFile(backups[c]);
+
+                String formattedMessage = String.format("Backup [%s] removed.", backups[c].getName());
+
+                ServerBackupPlugin.getPluginInstance().getLogger().info(formattedMessage);
+            } else {
+                String formattedMessage = String.format("No Backup named [%s] found.", backups[c].getName());
+
+                ServerBackupPlugin.getPluginInstance().getLogger().warning(formattedMessage);
             }
+
+            c++;
+            dobc++;
         }
     }
 
     private String getDayName(int dayNumber) {
-        if (dayNumber == 1) {
-            return "SUNDAY";
+        String[] daysOfWeek = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        if (dayNumber >= 1 && dayNumber <= 7) {
+            return daysOfWeek[dayNumber - 1];
         }
-
-        if (dayNumber == 2) {
-            return "MONDAY";
-        }
-
-        if (dayNumber == 3) {
-            return "TUESDAY";
-        }
-
-        if (dayNumber == 4) {
-            return "WEDNESDAY";
-        }
-
-        if (dayNumber == 5) {
-            return "THURSDAY";
-        }
-
-        if (dayNumber == 6) {
-            return "FRIDAY";
-        }
-
-        if (dayNumber == 7) {
-            return "SATURDAY";
-        }
-
         Bukkit.getLogger().log(Level.WARNING, "Error while converting number in day.");
-
         return null;
     }
-
 }
